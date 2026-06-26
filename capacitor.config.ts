5b65260545e8a3a5599e343630297c4662197af9
Be9He4 <Be9He4@users.noreply.github.com>
2026-06-26 23:28:34 +0800

    Initial commit: BH Music rebranding

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.be9he4.bhmusic',
+  appName: 'BH Music',
+  webDir: 'dist',
+};
+
+export default config;
